eb9dec7fc12be4ca9a435d5ecb3460c8df4218a2
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-03-27 22:39:04 -0300

    Move formatting to null-ls

--- .config/nvim/lua/plugins/null-ls.lua
+++ .config/nvim/lua/plugins/null-ls.lua
@@ -3,22 +3,37 @@ return {
     config = function()
         -- Sets a default apex ruleset path if the project doesn't have one
         local default_apex_ruleset = vim.env.HOME .. "/.local/bin/pmd-bin-7.0.0/rulesets/apex.xml"
+        local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
         local null_ls = require("null-ls")
         null_ls.setup({
+            on_attach = function(client, bufnr)
+                if client.supports_method("textDocument/formatting") then
+                    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                    vim.api.nvim_create_autocmd("BufWritePre", {
+                        group = augroup,
+                        buffer = bufnr,
+                        callback = function()
+                            -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                            -- on later neovim version, you should use vim.lsp.buf.format({ async = false }) instead
+                            vim.lsp.buf.format({ async = false })
+                        end,
+                    })
+                end
+            end,
             sources = {
+                -- Code Actions
+                null_ls.builtins.code_actions.gitsigns,   -- Actions for git hunks
+                -- Diagnostics
+                null_ls.builtins.diagnostics.actionlint,  -- Github actions linter
+                null_ls.builtins.diagnostics.commitlint,  -- Git commit linter
+                null_ls.builtins.diagnostics.trail_space, -- Trailing whitespace for all filetypes
                 null_ls.builtins.diagnostics.pmd.with({
-                    -- This overwrites the default args so pmd is not called on the entire project
+                    -- This requires having set up filetypes for apex and assigning file extensions
+                    -- This is done in sets.lua
+                    filetypes = { "apex" },
+                    -- This overwrites the default args so pmd is not called on the entire project,
+                    -- while adding extra args in the same function
                     args = function(params)
-                        return {
-                            "--format",
-                            "json",
-                            "--dir",
-                            -- Call pmd only on the current buffer and not the entire directory
-                            vim.api.nvim_buf_get_name(params.bufnr),
-                        }
-                    end,
-                    -- This are extra args besides the default overwritten ones
-                    extra_args = function(params)
                         -- Try using a local ruleset in .pmd/rulesets/apex.xml, or default one
                         local project_apex_ruleset = params.cwd .. "/.pmd/rulesets/apex.xml"
                         local ruleset
@@ -28,20 +43,39 @@ return {
                             ruleset = default_apex_ruleset
                         end
                         return {
+                            "--format",
+                            "json",
+                            "--dir",
+                            -- Call pmd only on the current buffer and not the entire directory
+                            vim.api.nvim_buf_get_name(params.bufnr),
                             "--rulesets",
                             ruleset,
                             "--no-cache",
-                            "--no-progress"
+                            "--no-progress",
                         }
                     end,
                     env = function(params)
                         return { PMD_APEX_ROOT_DIRECTORY = params.cwd }
                     end,
-                    -- This requires having set up filetypes for apex and assigning file extensions
-                    -- This is done in sets.lua
-                    filetypes = { "apex" },
                 }),
+                -- Formatting
+                null_ls.builtins.formatting.stylua.with({
+                    extra_args = { "--indent-width", "4", "--indent-type", "Spaces" },
+                }),
+                null_ls.builtins.formatting.prettier.with({
+                    extra_filetypes = { "apex" },
+                }),
+                null_ls.builtins.formatting.black,
+                null_ls.builtins.formatting.isort,
+                null_ls.builtins.formatting.shfmt,
             },
         })
     end,
+    commander = {
+        {
+            keys = { "n", "<leader>]" },
+            cmd = [[<cmd>lua vim.lsp.buf.format()<cr>]],
+            desc = "Format file",
+        },
+    },
 }
