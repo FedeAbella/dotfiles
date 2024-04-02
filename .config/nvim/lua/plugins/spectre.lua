181fedc474704d0ce4f963cf4bc54abfe70ed812
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-04-01 21:05:05 -0300

    Add spectre plugin to nvim

---
+++ .config/nvim/lua/plugins/spectre.lua
@@ -0,0 +1,46 @@
+return {
+    "nvim-pack/nvim-spectre",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    config = function()
+        require("spectre").setup({
+            find_engine = {
+                -- rg is map with finder_cmd
+                ["rg"] = {
+                    cmd = "rg",
+                    -- default args
+                    args = {
+                        "--color=never",
+                        "--no-heading",
+                        "--with-filename",
+                        "--line-number",
+                        "--column",
+                        "--follow",
+                    },
+                    options = {
+                        ["ignore-case"] = {
+                            value = "--ignore-case",
+                            icon = "[I]",
+                            desc = "ignore case",
+                        },
+                        ["hidden"] = {
+                            value = "--hidden",
+                            desc = "hidden file",
+                            icon = "[H]",
+                        },
+                        -- you can put any rg search option you want here it can toggle with
+                        -- show_option function
+                    },
+                },
+            },
+        })
+    end,
+    commander = {
+        {
+            keys = { "n", "<C-x>" },
+            cmd = [[<cmd>lua require'spectre'.toggle()<cr>]],
+            desc = "Spectre: Toggle",
+        },
+    },
+}
