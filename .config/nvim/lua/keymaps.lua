dcd26bb503430f2bb29402b418f81bb7a72bcfc5
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-04-10 11:59:07 -0300

    Add keymap for center view after move to end

--- .config/nvim/lua/keymaps.lua
+++ .config/nvim/lua/keymaps.lua
@@ -2,17 +2,17 @@ vim.g.mapleader = " "
 
 return {
     -- Buffers
-    { keys = { "n", "<A-c>" }, cmd = vim.cmd.bdelete, desc = "Buffers: Close current" },
-    { keys = { "n", "<C-h>" }, cmd = vim.cmd.bnext, desc = "Buffers: Next" },
-    { keys = { "n", "<C-l>" }, cmd = vim.cmd.bprevious, desc = "Buffers: Previous" },
-    { keys = { "n", "<A-C>" }, cmd = [[<cmd>%bd|e#|bd#<cr>|'"]], desc = "Buffers: Close others" },
-    { keys = { "n", "<leader>'" }, cmd = [[<C-^>]], desc = "Buffers: Last" },
+    { keys = { "n", "<A-c>" },      cmd = vim.cmd.bdelete,            desc = "Buffers: Close current" },
+    { keys = { "n", "<C-h>" },      cmd = vim.cmd.bnext,              desc = "Buffers: Next" },
+    { keys = { "n", "<C-l>" },      cmd = vim.cmd.bprevious,          desc = "Buffers: Previous" },
+    { keys = { "n", "<A-C>" },      cmd = [[<cmd>%bd|e#|bd#<cr>|'"]], desc = "Buffers: Close others" },
+    { keys = { "n", "<leader>'" },  cmd = [[<C-^>]],                  desc = "Buffers: Last" },
 
     -- Terminal
-    { keys = { "t", "<esc>" }, cmd = [[<C-\><C-n>]], desc = "Terminal: Quit", show = false },
+    { keys = { "t", "<esc>" },      cmd = [[<C-\><C-n>]],             desc = "Terminal: Quit",        show = false },
 
     -- Format
-    { keys = { "n", "<leader>\\" }, cmd = "50%ggVG=2<C-o>", desc = "Reindent file" },
+    { keys = { "n", "<leader>\\" }, cmd = "50%ggVG=2<C-o>",           desc = "Reindent file" },
     {
         keys = { "n", "<leader>w" },
         cmd = function()
@@ -22,23 +22,24 @@ return {
     },
 
     -- View
-    { keys = { "n", "<c-d>" }, cmd = "<c-d>zz", desc = "Move view down, but center view after", show = false },
-    { keys = { "n", "<c-u>" }, cmd = "<c-u>zz", desc = "Move view up, but center view after", show = false },
-    { keys = { "n", "N" }, cmd = "Nzz", desc = "Previous match, but center view after", show = false },
-    { keys = { "n", "n" }, cmd = "nzz", desc = "Next match, but center view after", show = false },
-    { keys = { "n", "%" }, cmd = "%zz", desc = "Go to matching parenthesis, but center view after", show = false },
-    { keys = { "n", "#" }, cmd = "#zz", desc = "Previous ocurrence, but center view after", show = false },
-    { keys = { "n", "*" }, cmd = "*zz", desc = "Next ocurrence, but center view after", show = false },
-    { keys = { "n", "{" }, cmd = "{zz", desc = "Previous paragraph, but center view after", show = false },
-    { keys = { "n", "}" }, cmd = "}zz", desc = "Next paragraph, but center view after", show = false },
+    { keys = { "n", "<c-d>" },     cmd = "<c-d>zz",          desc = "Move view down, but center view after",             show = false },
+    { keys = { "n", "<c-u>" },     cmd = "<c-u>zz",          desc = "Move view up, but center view after",               show = false },
+    { keys = { "n", "N" },         cmd = "Nzz",              desc = "Previous match, but center view after",             show = false },
+    { keys = { "n", "n" },         cmd = "nzz",              desc = "Next match, but center view after",                 show = false },
+    { keys = { "n", "%" },         cmd = "%zz",              desc = "Go to matching parenthesis, but center view after", show = false },
+    { keys = { "n", "#" },         cmd = "#zz",              desc = "Previous ocurrence, but center view after",         show = false },
+    { keys = { "n", "*" },         cmd = "*zz",              desc = "Next ocurrence, but center view after",             show = false },
+    { keys = { "n", "{" },         cmd = "{zz",              desc = "Previous paragraph, but center view after",         show = false },
+    { keys = { "n", "}" },         cmd = "}zz",              desc = "Next paragraph, but center view after",             show = false },
+    { keys = { "n", "G" },         cmd = "Gzz",              desc = "Go to end, but center view after",                  show = false },
 
     -- Editing
-    { keys = { "v", "J" }, cmd = ":m '>+1<cr>gv=gv", desc = "Move selection up" },
-    { keys = { "v", "K" }, cmd = ":m '<-2<cr>gv=gv", desc = "Move selection down" },
-    { keys = { "x", "<leader>p" }, cmd = '"_dP', desc = "Paste, but don't replace register" },
-    { keys = { "n", "<leader>y" }, cmd = '"+y', desc = "Yank into clipboard" },
-    { keys = { "v", "<leader>y" }, cmd = '"+y', desc = "Yank into clipboard" },
-    { keys = { "n", "<leader>Y" }, cmd = '"+Y', desc = "Yank lines into clipboard" },
+    { keys = { "v", "J" },         cmd = ":m '>+1<cr>gv=gv", desc = "Move selection up" },
+    { keys = { "v", "K" },         cmd = ":m '<-2<cr>gv=gv", desc = "Move selection down" },
+    { keys = { "x", "<leader>p" }, cmd = '"_dP',             desc = "Paste, but don't replace register" },
+    { keys = { "n", "<leader>y" }, cmd = '"+y',              desc = "Yank into clipboard" },
+    { keys = { "v", "<leader>y" }, cmd = '"+y',              desc = "Yank into clipboard" },
+    { keys = { "n", "<leader>Y" }, cmd = '"+Y',              desc = "Yank lines into clipboard" },
     {
         keys = {
             "n",
@@ -49,7 +50,7 @@ return {
     },
 
     -- Diagnostics
-    { keys = { "n", "<leader>dn" }, cmd = [[<cmd>lua vim.diagnostic.goto_next()<cr>]], desc = "Diagnostics: Next" },
+    { keys = { "n", "<leader>dn" }, cmd = [[<cmd>lua vim.diagnostic.goto_next()<cr>]],  desc = "Diagnostics: Next" },
     {
         keys = {
             "n",
