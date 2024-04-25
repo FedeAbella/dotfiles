d242b75abcd90991af65d172caa129f130758948
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-04-25 20:14:58 -0300

    Alias batcat

--- .local/config/.zshconfig.zsh
+++ .local/config/.zshconfig.zsh
@@ -3,7 +3,7 @@
 ## ALIASES
 
 # Edit the zsh shell configuration
-alias zshconfig='nvim ~/.zshrc' # edit the zshrc file
+alias zshconfig='nvim ~/.zshrc'                       # edit the zshrc file
 alias zshcustom='nvim ~/.local/config/.zshconfig.zsh' # edit the personalised config
 
 # require confirmations
@@ -18,6 +18,9 @@ alias ls='lsd'
 alias ll='ls -l'
 alias la='ls -a'
 alias lla='ls -al'
+
+## batcat alias
+alias bat='batcat'
 
 # Misc aliases
 [[ ! -f $HOME/.local/scripts/imwheelrc/mousewheel.sh ]] || alias mousewheel='~/.local/scripts/imwheelrc/mousewheel.sh' # edit mouse wheel speed
@@ -65,64 +68,60 @@ export RIPGREP_CONFIG_PATH=$HOME/.config/ripgrep/ripgreprc
 ## Navigation
 # Function to navigate up X directories
 # Taken from DT, modified heavily
-up () {
-    local d="" # start from an empty string
+up() {
+    local d=""     # start from an empty string
     local limit=$1 # get parameter
 
-        # error messages
-        local errNonInt="ERROR: up requires positive integer or no parameters"
-        local errOutBound="ERROR: parameter must be between 1 and 99"
+    # error messages
+    local errNonInt="ERROR: up requires positive integer or no parameters"
+    local errOutBound="ERROR: parameter must be between 1 and 99"
 
-        # Default to limit of 1 if null parameter
-        if [ -z $limit ]
-        then
-            limit=1
-        fi
+    # Default to limit of 1 if null parameter
+    if [ -z $limit ]; then
+        limit=1
+    fi
 
-        # throw error if parameter is not a number
-        (echo $limit | grep -Eq ^[0-9]+$) || (echo $errNonInt && return 1)
+    # throw error if parameter is not a number
+    (echo $limit | grep -Eq ^[0-9]+$) || (echo $errNonInt && return 1)
 
-        # throw error if parameter is not 1-99
-        if [[ $limit -ge 100 || $limit == 0 ]]
-        then
-            echo $errOutBound
-            return 1
-        fi
+    # throw error if parameter is not 1-99
+    if [[ $limit -ge 100 || $limit == 0 ]]; then
+        echo $errOutBound
+        return 1
+    fi
 
-        # construct cd string
-        for ((i=1;i<=limit;i++)); do
-            d="../$d"
-        done
+    # construct cd string
+    for ((i = 1; i <= limit; i++)); do
+        d="../$d"
+    done
 
-        cd $d # move
-    }
+    cd $d # move
+}
 
 ## lsd tree
 # function to call lsd --tree with a given depth more easily
-if [ alias lt >/dev/null 2>&1 ]; then
+if [ alias lt ] >/dev/null 2>&1; then
     unalias lt
 fi
-lt () {
+lt() {
     local depth=$1 # get parameter
 
-        # error messages
-        local errNonInt="ERROR: lt requires positive integer, or zero (for infinite depth)"
+    # error messages
+    local errNonInt="ERROR: lt requires positive integer, or zero (for infinite depth)"
 
-        # Default to limit of 1 if null parameter
-        if [ -z $depth ]
-        then
-            depth=1
-        fi
+    # Default to limit of 1 if null parameter
+    if [ -z $depth ]; then
+        depth=1
+    fi
 
-        # throw error if parameter is not a number
-        (echo $depth | grep -Eq ^[0-9]+$) || (echo $errNonInt && return 1)
+    # throw error if parameter is not a number
+    (echo $depth | grep -Eq ^[0-9]+$) || (echo $errNonInt && return 1)
 
-        if [ $depth -eq 0 ]
-        then
-            lsd --tree
-        else
-            lsd --tree --depth $depth
-        fi
+    if [ $depth -eq 0 ]; then
+        lsd --tree
+    else
+        lsd --tree --depth $depth
+    fi
 
-        return
-    }
+    return
+}
