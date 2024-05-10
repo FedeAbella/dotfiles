25d7650ec3df01fb2bd1bed14fb2f97288abb74d
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-05-10 18:05:57 -0300

    Remove tsserver format capabilities

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -57,6 +57,14 @@ return {
                         end,
                     })
                 end,
+                tsserver = function()
+                    require("lspconfig").tsserver.setup({
+                        on_init = function(client)
+                            client.server_capabilities.documentFormattingProvider = false
+                            client.server_capabilities.documentFormattingRangeProvider = false
+                        end,
+                    })
+                end,
             },
         })
 
