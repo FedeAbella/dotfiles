f108d1fab1ff2a85fc4ddb5bf83f5f568e87b0fb
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-07-16 10:39:45 -0300

    Add ignore blank lines to nvim/comment

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -2,7 +2,9 @@ return {
     "numToStr/Comment.nvim",
     lazy = false,
     config = function()
-        require("Comment").setup()
+        require("Comment").setup({
+            ignore = '^$'
+        })
         local ft = require("Comment.ft")
         ft.set("apex", { "//%s", "/*%s*/" })
     end,
