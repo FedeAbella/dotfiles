708c78abbe07bcf377ef68f67dbd0fbc7c38683e
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-09-08 21:58:01 -0300

    Add fzf default command to be fd

--- .local/config/.zshconfig.zsh
+++ .local/config/.zshconfig.zsh
@@ -31,6 +31,7 @@ if [[ -n $(command -v fzf) ]]; then
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
     --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
     --multi"
+    export FZF_DEFAULT_COMMAND='fd --hidden --strip-cwd-prefix --exclude .git'
 fi
 
 # Script aliases
