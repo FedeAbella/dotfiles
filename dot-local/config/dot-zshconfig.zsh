70977284236f33b93c68eb682e1ff39acd1efdc4
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-02 11:51:45 -0300

    Add zsh histfile location

--- dot-local/config/dot-zshconfig.zsh
+++ dot-local/config/dot-zshconfig.zsh
@@ -3,8 +3,10 @@
 # XDG envs
 export XDG_CONFIG_HOME="$HOME"/.config
 export XDG_DATA_HOME="$HOME"/.local/share
+export XDG_STATE_HOME="$HOME"/.local/state
 export CARGO_HOME="$XDG_DATA_HOME"/cargo
 export GTK2_RC_FILES="$XDG_CONFIG_HOME/gtk-2.0/gtkrc":"$XDG_CONFIG_HOME/gtk-2.0/gtkrc.mine"
+HISTFILE="$XDG_STATE_HOME"/zsh/history
 
 ## ALIASES
 
