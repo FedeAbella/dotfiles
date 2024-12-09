60dfd465bb01b9de85ba49fcd6b7abfc49d92a2a
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-09 11:12:40 -0300

    Add lazydev.nvim plugin

--- dot-config/nvim/lua/plugins/nvim-lspconfig.lua
+++ dot-config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -4,8 +4,28 @@ return {
     cmd = { "LspInfo", "LspInstall", "LspStart" },
     event = { "BufReadPre", "BufNewFile" },
     dependencies = {
-        { "hrsh7th/cmp-nvim-lsp" },
+        {
+            "hrsh7th/cmp-nvim-lsp",
+            opts = function(_, opts)
+                opts.sources = opts.sources or {}
+                table.insert(opts.sources, {
+                    name = "lazydev",
+                    group_index = 0, -- set group index to 0 to skip loading LuaLS completions
+                })
+            end,
+        },
         { "williamboman/mason-lspconfig.nvim" },
+        {
+            "folke/lazydev.nvim",
+            ft = "lua", -- only load on lua files
+            opts = {
+                library = {
+                    -- See the configuration section for more details
+                    -- Load luvit types when the `vim.uv` word is found
+                    { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                },
+            },
+        },
     },
     config = function()
         -- This is where all the LSP shenanigans will live
