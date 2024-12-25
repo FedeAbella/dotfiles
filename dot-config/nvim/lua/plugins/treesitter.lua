42b9a5a4c653cc340adf4970f007842a5f11232a
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-25 15:52:08 -0300

    move context plugin to treesitter file

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,41 +1,46 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {
-                "apex",
-                "bash",
-                "sosl",
-                "soql",
-                "sflog",
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "query",
-                "elixir",
-                "heex",
-                "javascript",
-                "html",
-                "markdown",
-                "xml",
-                "python",
-                "regex",
-                "markdown_inline",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = {
+                    "apex",
+                    "bash",
+                    "sosl",
+                    "soql",
+                    "sflog",
+                    "c",
+                    "lua",
+                    "vim",
+                    "vimdoc",
+                    "query",
+                    "elixir",
+                    "heex",
+                    "javascript",
+                    "html",
+                    "markdown",
+                    "xml",
+                    "python",
+                    "regex",
+                    "markdown_inline",
+                },
+                sync_install = false,
+                highlight = { enable = true },
+                indent = { enable = true },
+            })
+        end,
+        dependencies = {
+            {
+                "HiPhish/rainbow-delimiters.nvim",
+                config = function()
+                    require("rainbow-delimiters")
+                    require("rainbow-delimiters.setup").setup()
+                end,
             },
-            sync_install = false,
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end,
-    dependencies = {
-        {
-            "HiPhish/rainbow-delimiters.nvim",
-            config = function()
-                require("rainbow-delimiters")
-                require("rainbow-delimiters.setup").setup()
-            end,
         },
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
     },
 }
