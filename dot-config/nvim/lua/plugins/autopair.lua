56a3183cccda749f33949f19441714a552564c2a
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-25 15:55:20 -0300

    move autotags to autopair file

--- dot-config/nvim/lua/plugins/autopair.lua
+++ dot-config/nvim/lua/plugins/autopair.lua
@@ -1,5 +1,16 @@
 return {
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = true,
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+    },
+    {
+        "windwp/nvim-ts-autotag",
+        dependencies = "nvim-treesitter/nvim-treesitter",
+        config = function()
+            require("nvim-ts-autotag").setup()
+        end,
+        lazy = true,
+        event = "VeryLazy",
+    },
 }
