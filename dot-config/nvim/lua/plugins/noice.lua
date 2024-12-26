d1cfd6ce0c98897675dedc95ac981ce64b74f318
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-26 17:12:18 -0300

    clean up noice.nvim

--- dot-config/nvim/lua/plugins/noice.lua
+++ dot-config/nvim/lua/plugins/noice.lua
@@ -1,40 +1,39 @@
 return {
-    "folke/noice.nvim",
-    event = "VeryLazy",
-    dependencies = {
-        -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
-        "MunifTanjim/nui.nvim",
-        -- OPTIONAL:
-        --   `nvim-notify` is only needed, if you want to use the notification view.
-        --   If not available, we use `mini` as the fallback
-        "rcarriga/nvim-notify",
+  "folke/noice.nvim",
+  lazy = true,
+  event = "VeryLazy",
+  dependencies = {
+    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+    "MunifTanjim/nui.nvim",
+    -- OPTIONAL:
+    --   `nvim-notify` is only needed, if you want to use the notification view.
+    --   If not available, we use `mini` as the fallback
+    "rcarriga/nvim-notify",
+  },
+  opts = {
+    lsp = {
+      progress = {
+        enabled = false,
+      },
+      hover = {
+        enabled = false,
+      },
+      signature = {
+        enabled = false,
+      },
     },
-    config = function()
-        require("noice").setup({
-            lsp = {
-                progress = {
-                    enabled = false,
-                },
-                hover = {
-                    enabled = false,
-                },
-                signature = {
-                    enabled = false,
-                },
-            },
-            presets = {
-                bottom_search = false,
-                command_palette = true,
-            },
-            routes = {
-                {
-                    view = "notify",
-                    filter = { event = "msg_showmode" },
-                    opts = {
-                        timeout = 6000,
-                    },
-                },
-            },
-        })
-    end,
+    presets = {
+      bottom_search = false,
+      command_palette = true,
+    },
+    routes = {
+      {
+        view = "notify",
+        filter = { event = "msg_showmode" },
+        opts = {
+          timeout = 6000,
+        },
+      },
+    },
+  },
 }
