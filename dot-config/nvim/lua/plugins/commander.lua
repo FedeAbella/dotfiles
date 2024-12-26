f1a020595900afefac66169f852738f240084fef
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-26 16:04:31 -0300

    reformat commander.nvim

--- dot-config/nvim/lua/plugins/commander.lua
+++ dot-config/nvim/lua/plugins/commander.lua
@@ -1,34 +1,34 @@
 return {
-    "FeiyouG/commander.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    config = function()
-        local commander = require("commander")
-        commander.setup({
-            components = {
-                "DESC",
-                "KEYS",
-                "CAT",
-            },
-            sort_by = {
-                "DESC",
-                "KEYS",
-                "CAT",
-                "CMD",
-            },
-            integration = {
-                lazy = {
-                    enable = true,
-                    set_plugin_name_as_cat = true,
-                },
-            },
-        })
-        commander.add({
-            {
-                desc = "Commander: Open",
-                cmd = commander.show,
-                keys = { "n", "<leader>k" },
-            },
-        })
-        commander.add(require("keymaps"))
-    end,
+  "FeiyouG/commander.nvim",
+  dependencies = { "nvim-telescope/telescope.nvim" },
+  config = function()
+    local commander = require("commander")
+    commander.setup({
+      components = {
+        "DESC",
+        "KEYS",
+        "CAT",
+      },
+      sort_by = {
+        "DESC",
+        "KEYS",
+        "CAT",
+        "CMD",
+      },
+      integration = {
+        lazy = {
+          enable = true,
+          set_plugin_name_as_cat = true,
+        },
+      },
+    })
+    commander.add({
+      {
+        desc = "Commander: Open",
+        cmd = commander.show,
+        keys = { "n", "<leader>k" },
+      },
+    })
+    commander.add(require("keymaps"))
+  end,
 }
