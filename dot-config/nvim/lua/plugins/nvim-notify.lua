b53c37acd63f7ae2991e52eac370cfe99e951477
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-26 17:41:00 -0300

    clean up notify.nvim

--- dot-config/nvim/lua/plugins/nvim-notify.lua
+++ dot-config/nvim/lua/plugins/nvim-notify.lua
@@ -1,11 +1,8 @@
 return {
-    "rcarriga/nvim-notify",
-    config = function()
-        require("notify").setup({
-            max_width = 50,
-            render = "compact",
-            stages = "fade",
-            timeout = 4000,
-        })
-    end,
+  "rcarriga/nvim-notify",
+  opts = {
+    render = "compact",
+    stages = "fade",
+    timeout = 4000,
+  },
 }
