6e22f4c2a5965dd8082fa1ec0145e03ef33f2239
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-26 16:02:22 -0300

    reformat catppuccin

--- dot-config/nvim/lua/plugins/catppuccin.lua
+++ dot-config/nvim/lua/plugins/catppuccin.lua
@@ -1,49 +1,51 @@
 return {
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        lazy = false,
-        config = function()
-            local mocha = require("catppuccin.palettes.mocha")
-            require("catppuccin").setup({
-                dim_inactive = {
-                    enabled = true,
-                },
-                integrations = {
-                    notify = true,
-                    mason = true,
-                },
-                custom_highlights = function(colors)
-                    return {
-                        LineNr = { fg = colors.overlay1 },
-                        TelescopeBorder = { fg = colors.mauve },
-                        TelescopeMatching = { fg = colors.teal },
-                        TelescopeSelection = { fg = colors.maroon },
-                        TelescopeSelectionCaret = { fg = colors.maroon },
-                        NeoTreeDirectoryName = { fg = colors.mauve },
-                        NeoTreeDirectoryIcon = { fg = colors.mauve },
-                        NeoTreeRootName = { fg = colors.mauve },
-                        NeoTreeGitUntracked = { fg = colors.sapphire },
-                        NotifyINFOBorder = { fg = colors.mauve },
-                        NotifyINFOIcon = { fg = colors.mauve },
-                        NotifyINFOTitle = { fg = colors.mauve, style = { "italic" } },
-                        Whitespace = { fg = colors.overlay1 },
-                        FloatBorder = { fg = colors.mauve },
-                        GitSignsCurrentLineBlame = { fg = colors.overlay1 },
-                        DiagnosticVirtualTextError = { bg = colors.none },
-                        DiagnosticVirtualTextWarn = { bg = colors.none },
-                        DiagnosticVirtualTextInfo = { bg = colors.none },
-                        DiagnosticVirtualTextHint = { bg = colors.none },
-                        DiffChange = { bg = colors.green, fg = colors.surface1 },
-                        DiffDelete = { bg = colors.red, fg = colors.surface1 },
-                    }
-                end,
-            })
-            vim.cmd.colorscheme("catppuccin-mocha")
-            vim.api.nvim_set_hl(0, "OilDir", { fg = mocha.mauve })
-            vim.api.nvim_set_hl(0, "OilLink", { fg = mocha.peach })
-            vim.api.nvim_set_hl(0, "OilLinkTarget", { fg = mocha.rosewater })
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    lazy = false,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        dim_inactive = {
+          enabled = true,
+        },
+        integrations = {
+          notify = true,
+          mason = true,
+        },
+        custom_highlights = function(colors)
+          return {
+            LineNr = { fg = colors.overlay1 },
+            TelescopeBorder = { fg = colors.mauve },
+            TelescopeMatching = { fg = colors.teal },
+            TelescopeSelection = { fg = colors.maroon },
+            TelescopeSelectionCaret = { fg = colors.maroon },
+            NeoTreeDirectoryName = { fg = colors.mauve },
+            NeoTreeDirectoryIcon = { fg = colors.mauve },
+            NeoTreeRootName = { fg = colors.mauve },
+            NeoTreeGitUntracked = { fg = colors.sapphire },
+            NotifyINFOBorder = { fg = colors.mauve },
+            NotifyINFOIcon = { fg = colors.mauve },
+            NotifyINFOTitle = { fg = colors.mauve, style = { "italic" } },
+            Whitespace = { fg = colors.overlay1 },
+            FloatBorder = { fg = colors.mauve },
+            GitSignsCurrentLineBlame = { fg = colors.overlay1 },
+            DiagnosticVirtualTextError = { bg = colors.none },
+            DiagnosticVirtualTextWarn = { bg = colors.none },
+            DiagnosticVirtualTextInfo = { bg = colors.none },
+            DiagnosticVirtualTextHint = { bg = colors.none },
+            DiffChange = { bg = colors.green, fg = colors.surface1 },
+            DiffDelete = { bg = colors.red, fg = colors.surface1 },
+          }
         end,
-    },
+      })
+
+      local mocha = require("catppuccin.palettes.mocha")
+      vim.cmd.colorscheme("catppuccin")
+      vim.api.nvim_set_hl(0, "OilDir", { fg = mocha.mauve })
+      vim.api.nvim_set_hl(0, "OilLink", { fg = mocha.peach })
+      vim.api.nvim_set_hl(0, "OilLinkTarget", { fg = mocha.rosewater })
+    end,
+  },
 }
