3884d7748e6b7ea3a4edfbae3dead9ed4ceac512
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-26 16:47:11 -0300

    clean up comment.nvim

--- dot-config/nvim/lua/plugins/comment.lua
+++ dot-config/nvim/lua/plugins/comment.lua
@@ -2,11 +2,7 @@ return {
   "numToStr/Comment.nvim",
   lazy = true,
   event = "VeryLazy",
-  config = function()
-    require("Comment").setup({
-      ignore = "^$",
-    })
-    local ft = require("Comment.ft")
-    ft.set("apex", { "//%s", "/*%s*/" })
-  end,
+  opts = {
+    ignore = "^$",
+  },
 }
