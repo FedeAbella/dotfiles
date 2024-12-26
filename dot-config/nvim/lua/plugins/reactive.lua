fd2e95fc8486de7800769609124b20a123be8eb2
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-26 20:23:04 -0300

    clean up reactive.nvim

--- dot-config/nvim/lua/plugins/reactive.lua
+++ dot-config/nvim/lua/plugins/reactive.lua
@@ -1,12 +1,10 @@
 -- Makes the cursor and cursorline change depending on mode
 return {
-    "rasulomaroff/reactive.nvim",
-    config = function()
-        require("reactive").setup({
-            load = {
-                "catppuccin-mocha-cursor",
-                "catppuccin-mocha-cursorline",
-            },
-        })
-    end,
+  "rasulomaroff/reactive.nvim",
+  opts = {
+    load = {
+      "catppuccin-mocha-cursor",
+      "catppuccin-mocha-cursorline",
+    },
+  },
 }
