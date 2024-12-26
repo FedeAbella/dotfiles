49f08821a22d1f82072f0baab81abc29389d2410
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-26 20:20:40 -0300

    reformat oil.nvim

--- dot-config/nvim/lua/plugins/oil.lua
+++ dot-config/nvim/lua/plugins/oil.lua
@@ -1,26 +1,26 @@
 return {
-    "stevearc/oil.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {
-        columns = {
-            "icon",
-            "size",
-            "mtime",
-        },
-        delete_to_trash = true,
-        constrain_cursor = "name",
-        watch_for_changes = true,
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+    columns = {
+      "icon",
+      "size",
+      "mtime",
     },
-    commander = {
-        {
-            keys = { "n", "<leader>-" },
-            cmd = [[<cmd>Oil --float .<cr>]],
-            desc = "Oil: Open root",
-        },
-        {
-            keys = { "n", "<leader>_" },
-            cmd = [[<cmd>Oil --float<cr>]],
-            desc = "Oil: Open here",
-        },
+    delete_to_trash = true,
+    constrain_cursor = "name",
+    watch_for_changes = true,
+  },
+  commander = {
+    {
+      keys = { "n", "<leader>-" },
+      cmd = [[<cmd>Oil --float .<cr>]],
+      desc = "Oil: Open root",
     },
+    {
+      keys = { "n", "<leader>_" },
+      cmd = [[<cmd>Oil --float<cr>]],
+      desc = "Oil: Open here",
+    },
+  },
 }
