770d301db01f692254a08dcce9524009180e8b77
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-27 00:12:02 -0300

    clean up spectre

--- dot-config/nvim/lua/plugins/spectre.lua
+++ dot-config/nvim/lua/plugins/spectre.lua
@@ -1,25 +1,23 @@
 return {
-    "nvim-pack/nvim-spectre",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
+  "nvim-pack/nvim-spectre",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+  lazy = true,
+  cmd = "Spectre",
+  opts = {
+    find_engine = {
+      -- rg is map with finder_cmd
+      ["rg"] = {
+        args = {
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--follow",
+        },
+      },
     },
-    lazy = true,
-    cmd = "Spectre",
-    config = function()
-        require("spectre").setup({
-            find_engine = {
-                -- rg is map with finder_cmd
-                ["rg"] = {
-                    args = {
-                        "--color=never",
-                        "--no-heading",
-                        "--with-filename",
-                        "--line-number",
-                        "--column",
-                        "--follow",
-                    },
-                },
-            },
-        })
-    end,
+  },
 }
