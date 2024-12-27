56b5e465f0d0d1fde70a70e470526d270b0a918b
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2024-12-27 12:57:15 -0300

    clean up treesitter

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -2,42 +2,37 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    dependencies = {
-      {
-        "HiPhish/rainbow-delimiters.nvim",
-        config = function()
-          require("rainbow-delimiters")
-          require("rainbow-delimiters.setup").setup()
-        end,
+    main = "nvim-treesitter.configs",
+    opts = {
+      auto_install = false,
+      ensure_installed = {
+        "apex",
+        "bash",
+        "sosl",
+        "soql",
+        "sflog",
+        "lua",
+        "vim",
+        "vimdoc",
+        "query",
+        "javascript",
+        "html",
+        "markdown",
+        "xml",
+        "python",
+        "regex",
+        "markdown_inline",
       },
+      highlight = { enable = true },
+      indent = { enable = true },
     },
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        auto_install = false,
-        ensure_installed = {
-          "apex",
-          "bash",
-          "sosl",
-          "soql",
-          "sflog",
-          "lua",
-          "vim",
-          "vimdoc",
-          "query",
-          "javascript",
-          "html",
-          "markdown",
-          "xml",
-          "python",
-          "regex",
-          "markdown_inline",
-        },
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
   },
   {
     "nvim-treesitter/nvim-treesitter-context",
+  },
+  {
+    "HiPhish/rainbow-delimiters.nvim",
+    main = "rainbow-delimiters.setup",
+    config = true,
   },
 }
