58313ea613296702d9e6fdcf028dea5f3a98d8f9
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-01-12 17:50:29 -0300

    fix attentis startup apps

--- dot-config/hypr/scripts/startup-apps.sh
+++ dot-config/hypr/scripts/startup-apps.sh
@@ -11,9 +11,10 @@ case $user in
         hyprctl dispatch exec "[workspace 1] firefox"
         ;;
     attentis)
-        hyprctl dispatch exec "[workspace 2 silent] chromium --profile-directory='Profile 1'"
-        teams-for-linux &
-        flatpak run com.rtosta.zapzap &
+        hyprctl dispatch exec "[workspace 6 silent] teams-for-linux"
+        hyprctl dispatch exec "[workspace 3 silent] chromium --profile-directory='Profile 1'"
+        hyprctl dispatch exec "[workspace 2 silent] kitty"
+        hyprctl dispatch exec "[workspace 6 silent] flatpak run com.rtosta.zapzap"
         sleep 3
         hyprctl dispatch exec "[workspace 1] chromium --profile-directory=Default"
         ;;
