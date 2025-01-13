6aa8c5b12f9afdd385a9d74475883d4cf7fd0da4
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-01-13 12:01:41 -0300

    remove plus sign from custom weather temp

--- dot-config/waybar/custom/weather.sh
+++ dot-config/waybar/custom/weather.sh
@@ -3,7 +3,7 @@
 location="Montevideo+Uruguay"
 
 for i in {1..5}; do
-    if weather="$(curl -s https://wttr.in/$location?format=%c+%t+%p+%w | sed 's/\s\+/ /g')"; then
+    if weather="$(curl -s https://wttr.in/$location?format=%c+%t+%p+%w | sed 's/\s\+/ /g' | sed 's/+//g')"; then
         jq -c -M --arg weather "$weather" '.icon = ($weather | split(" ") | .[0]) | .temp = "🌡️" + ($weather | split(" ") | .[1]) | .prec = "☔" + ($weather | split(" ") | .[2]) | .wind = "🍃" + ($weather | split(" ") | .[3]) | .text = .icon + " " + .temp + " " + .prec + " " + .wind | {text}' < <(echo '{}')
         exit
     fi
