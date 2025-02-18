79b7ae95fa1914d090b1c83319836077b34bdaa7
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-02-18 16:53:52 -0300

    add headers to gb script

--- dot-local/scripts/git-branches.sh
+++ dot-local/scripts/git-branches.sh
@@ -3,51 +3,36 @@
 function listBranchWithDescription() {
     branches=$(git branch --list $1)
 
-  # requires git > v.1.7.9
+    print_string="\033[31;1mBranch\033[0m\t\033[31;1mRemote\033[0m\t\033[31;1mDescription\033[0m\n"
 
-  # you can set branch's description using command
-  # git branch --edit-description
-  # this opens the configured text editor, enter message, save and exit
-  # if one editor does not work (for example Sublime does not work for me)
-  # try another one, like vi
+    while read -r branch; do
+        # git marks current branch with "* ", remove it
+        clean_branch_name=${branch//\*\ /}
+        # replace colors
+        clean_branch_name=$(echo $clean_branch_name | tr -d '[:cntrl:]' | sed -E "s/\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
+        # replace symbolic-ref like `HEAD -> master`
+        clean_branch_name=$(echo $clean_branch_name | sed -E "s/^.+ -> //g")
 
-  # you can see branch's description using
-  # git config branch.<branch name>.description
+        description=$(git config branch.$clean_branch_name.description)
+        remote=$(git config branch.$clean_branch_name.remote)
+        merge=$(git config branch.$clean_branch_name.merge | cut --delimiter="/" --fields=3-)
 
-  print_string=""
-  while read -r branch; do
-      # git marks current branch with "* ", remove it
-      clean_branch_name=${branch//\*\ /}
-      # replace colors
-      clean_branch_name=$(echo $clean_branch_name | tr -d '[:cntrl:]' | sed -E "s/\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g")
-      # replace symbolic-ref like `HEAD -> master`
-      clean_branch_name=$(echo $clean_branch_name | sed -E "s/^.+ -> //g")
+        if [ "${branch::1}" == "*" ]; then
+            print_string="${print_string}${branch}"
+        else
+            print_string="${print_string}  ${branch}"
+        fi
 
-      description=$(git config branch.$clean_branch_name.description)
-      remote=$(git config branch.$clean_branch_name.remote)
-      merge=$(git config branch.$clean_branch_name.merge | cut --delimiter="/" --fields=3-)
+        if [ ! -z "$remote" ] && [ ! -z "$merge" ]; then
+            print_string="${print_string}\t\e[1;34m[${remote}/${merge}]\e[0m"
+        else
+            print_string="${print_string}\t"
+        fi
 
-      if [ "${branch::1}" == "*" ]; then
-          print_string="${print_string}${branch}"
-      else
-          print_string="${print_string}  ${branch}"
-      fi
+        print_string="${print_string}\t${description}\n"
+    done <<<"$branches"
 
-      if [ ! -z "$remote" ] && [ ! -z "$merge" ]; then
-          print_string="${print_string}\t\e[1;34m[${remote}/${merge}]\e[0m"
-      else
-          print_string="${print_string}\t"
-      fi
-
-      print_string="${print_string}\t${description}\n"
-  done <<< "$branches"
-
-  printf "$print_string" | column --table --separator $'\t'
-
-  # example output
-  # $ ./branches.sh
-  # * master        this is master branch
-  # one             this is simple branch for testing
+    printf "$print_string" | column --table --separator $'\t'
 }
 
 # @see [git-branch](https://git-scm.com/docs/git-branch)
