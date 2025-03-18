2dc8d9d9f53a0f0442e0a06870fc4d315fab376c
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-03-18 14:41:25 -0300

    replace firefox with zen browser in startup apps

--- dot-config/hypr/scripts/startup-apps.sh
+++ dot-config/hypr/scripts/startup-apps.sh
@@ -8,13 +8,13 @@ case $user in
         hyprctl dispatch exec "[workspace 7 silent] discord"
         /usr/bin/steam-runtime -silent &
         sleep 3
-        hyprctl dispatch exec "[workspace 1] firefox"
+        hyprctl dispatch exec "[workspace 1] flatpak run app.zen_browser.zen"
         ;;
     attentis)
         hyprctl dispatch exec "[workspace 7 silent] teams-for-linux"
         hyprctl dispatch exec "[workspace 4 silent] chromium --profile-directory='Profile 1'"
         hyprctl dispatch exec "[workspace 2 silent] kitty"
-        hyprctl dispatch exec "[workspace 9 silent] firefox"
+        hyprctl dispatch exec "[workspace 9 silent] flatpak run app.zen_browser.zen"
         hyprctl dispatch exec "[workspace 7 silent] flatpak run com.rtosta.zapzap"
         sleep 3
         hyprctl dispatch exec "[workspace 1] chromium --profile-directory=Default"
