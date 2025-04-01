249ab3b2d62bda7ed00e6418d72c7a8b88355634
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-04-01 11:39:54 -0300

    revert brave back to chromium

--- dot-config/hypr/scripts/startup-apps.sh
+++ dot-config/hypr/scripts/startup-apps.sh
@@ -12,12 +12,12 @@ case $user in
         ;;
     attentis)
         hyprctl dispatch exec "[workspace 7 silent] teams-for-linux"
-        hyprctl dispatch exec "[workspace 4 silent] brave --profile-directory='Profile 2' --password-store=detect"
+        hyprctl dispatch exec "[workspace 4 silent] chromium --profile-directory='Profile 1'"
         hyprctl dispatch exec "[workspace 2 silent] kitty"
         hyprctl dispatch exec "[workspace 9 silent] firefox"
         hyprctl dispatch exec "[workspace 7 silent] flatpak run com.rtosta.zapzap"
         sleep 3
-        hyprctl dispatch exec "[workspace 1] brave --profile-directory='Profile 1' --password-store=detect"
+        hyprctl dispatch exec "[workspace 1] chromium --profile-directory='Default'"
         ;;
     *) ;;
 esac
