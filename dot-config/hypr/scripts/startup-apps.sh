0b0b8b68cdc87c2892d19f237c2915b4ecc1a6ba
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-04-01 11:25:53 -0300

    revert from zen browser to firefox

--- dot-config/hypr/scripts/startup-apps.sh
+++ dot-config/hypr/scripts/startup-apps.sh
@@ -8,13 +8,13 @@ case $user in
         hyprctl dispatch exec "[workspace 7 silent] discord"
         /usr/bin/steam-runtime -silent &
         sleep 3
-        hyprctl dispatch exec "[workspace 1] flatpak run app.zen_browser.zen"
+        hyprctl dispatch exec "[workspace 1] firefox"
         ;;
     attentis)
         hyprctl dispatch exec "[workspace 7 silent] teams-for-linux"
         hyprctl dispatch exec "[workspace 4 silent] brave --profile-directory='Profile 2' --password-store=detect"
         hyprctl dispatch exec "[workspace 2 silent] kitty"
-        hyprctl dispatch exec "[workspace 9 silent] flatpak run app.zen_browser.zen"
+        hyprctl dispatch exec "[workspace 9 silent] firefox"
         hyprctl dispatch exec "[workspace 7 silent] flatpak run com.rtosta.zapzap"
         sleep 3
         hyprctl dispatch exec "[workspace 1] brave --profile-directory='Profile 1' --password-store=detect"
