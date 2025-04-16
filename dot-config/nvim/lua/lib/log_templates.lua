a9df71e5032f31f878f279be7553a197135fcba2
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-04-16 12:06:30 -0300

    add apex log template

--- dot-config/nvim/lua/lib/log_templates.lua
+++ dot-config/nvim/lua/lib/log_templates.lua
@@ -1,3 +1,4 @@
 return {
-  javascript = 'norm oconsole.log("%s: la, %sA;',
+  javascript = 'norm oconsole.log("%s: ", %s);',
+  apex = "norm oSystem.debug(String.format('%s: {0}', new List<String>{String.valueOf(%s)}));",
 }
