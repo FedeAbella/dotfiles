e791d33a11eff774e17a19ffdad2f2e74730d324
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-05-25 17:12:29 -0300

    add nvim alias

--- dot-config/zsh/aliases.zsh
+++ dot-config/zsh/aliases.zsh
@@ -1,8 +1,5 @@
 #Zsh aliases
 
-# nvim in Ubuntu
-[[ $(command -v nvim) ]] || [[ ! -d $HOME/.local/bin/nvim-linux64/bin ]] || alias nvim='$HOME/.local/bin/nvim-linux64/bin/nvim'
-
 # Edit the zsh shell configuration
 alias zshconfig='nvim -O $XDG_CONFIG_HOME/zsh/.zshrc $XDG_CONFIG_HOME/zsh/aliases.zsh $XDG_CONFIG_HOME/zsh/dirs.zsh $XDG_CONFIG_HOME/zsh/apps.zsh'
 
@@ -15,23 +12,26 @@ alias rm='nocorrect rm -i'
 
 alias mkdir='nocorrect mkdir -pv'
 
-## lsd aliases
+## lsd
 alias ls='lsd -1'
 alias ll='lsd -l'
 alias la='lsd -a'
 alias lla='lsd -al'
 
-## kitty aliases
+## kitty
 alias icat='kitten icat'
 
-## pacman aliases
+## pacman
 alias pacman-cleanup='sudo pacman -Qdtq | sudo pacman -Rns -'
 
-## hyprland aliases
+## hyprland
 alias hyprconfig='nvim $HOME/.config/hypr/hyprland.conf'
 
-# Script aliases
+# git
 alias gb='bash ~/.local/scripts/git/branches.sh'
 alias gnb='bash ~/.local/scripts/git/newbranch.sh'
 alias grb='bash ~/.local/scripts/git/rebasefrom.sh'
 alias gsp='bash ~/.local/scripts/git/switchandpull.sh'
+
+# nvim
+alias nv='nvim'
