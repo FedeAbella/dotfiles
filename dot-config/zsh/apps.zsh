de3c1108151c2bc59a5e767054fd7ab38bafe472
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-07-31 11:38:19 -0300

    bind preview scrolling in fzf

--- dot-config/zsh/apps.zsh
+++ dot-config/zsh/apps.zsh
@@ -8,7 +8,9 @@ if command -v fzf >/dev/null; then
     --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
     --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
     --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-    --multi"
+    --multi \
+    --bind 'ctrl-u:preview-half-page-up' \
+    --bind 'ctrl-d:preview-half-page-down'"
     export FZF_CTRL_T_OPTS=" \
     --walker-skip SteamLibrary,.steam,.git,node_modules \
     --preview=\"bat --color=always --style=numbers --line-range=:500 {}\""
