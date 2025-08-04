672d58589ace599aeb077e2e998013a2aa45f123
Federico Abella <65520926+FedeAbella@users.noreply.github.com>
2025-08-04 12:11:59 -0300

    add aura enabled property snippet and fix indentation

--- dot-config/nvim/snippets/all.lua
+++ dot-config/nvim/snippets/all.lua
@@ -1,13 +1,13 @@
 return {
-    s({
-        trig = "ternary",
-        desc = "A ternary operator",
-        docstring = "Ternary Operator: {1:cond} ? {2:then} : {3:else}",
-    }, {
-        i(1, "cond"),
-        t(" ? "),
-        i(2, "then"),
-        t(" : "),
-        i(3, "else"),
-    }),
+  s({
+    trig = "ternary",
+    desc = "A ternary operator",
+    docstring = "Ternary Operator: {1:cond} ? {2:then} : {3:else}",
+  }, {
+    i(1, "cond"),
+    t(" ? "),
+    i(2, "then"),
+    t(" : "),
+    i(3, "else"),
+  }),
 }
